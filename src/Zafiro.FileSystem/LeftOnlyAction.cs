700b838d937cccb26540c5b76ecbd9c7edfbce0b
José Manuel Nieto <superjmn@outlook.com>
2023-08-04 01:17:29 +0200

    Projects

---
+++ src/Zafiro.FileSystem/LeftOnlyAction.cs
@@ -0,0 +1,61 @@
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using CSharpFunctionalExtensions;
+using Zafiro.Functional;
+using Zafiro.IO;
+using Zafiro.ProgressReporting;
+using ObservableEx = Zafiro.Mixins.ObservableEx;
+
+namespace Zafiro.FileSystem;
+
+public class LeftOnlyAction : ISyncAction
+{
+    private readonly ISubject<RelativeProgress<long>> progressSubject = new Subject<RelativeProgress<long>>();
+
+    public LeftOnlyAction(IZafiroFile source, IZafiroFile destination)
+    {
+        Source = source;
+        Destination = destination;
+    }
+
+    public IZafiroFile Source { get; }
+
+    public IZafiroFile Destination { get; }
+
+    public IObservable<RelativeProgress<long>> Progress => progressSubject.AsObservable();
+
+    public IObservable<Result> Sync()
+    {
+        return Observable
+            .FromAsync(Source.GetContents)
+            .Successes()
+            .SelectMany(stream => ObservableEx.Using(() => GetInputStream(Source, stream), obs =>
+                Observable.FromAsync(async () =>
+                {
+                    var onNext = obs.Positions.Select(l => new RelativeProgress<long>(obs.Length, l));
+                    using (onNext.Subscribe(progressSubject))
+                    {
+                        var contents = await Destination.SetContents(obs);
+                        progressSubject.OnNext(new RelativeProgress<long>(obs.Length, obs.Length));
+                        return contents;
+                    }
+                })))
+            .FirstAsync();
+    }
+
+    private static async Task<ObservableStream> GetInputStream(IZafiroFile zafiroFile, Stream stream)
+    {
+        Stream inner;
+        if (!stream.CanSeek)
+        {
+            var size = await zafiroFile.Size();
+            inner = new AlwaysForwardStream(stream, size);
+        }
+        else
+        {
+            inner = stream;
+        }
+
+        return new ObservableStream(inner);
+    }
+}
---
+++ src/Zafiro.FileSystem/RightOnlyAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Reactive.Linq;
+using CSharpFunctionalExtensions;
+using Zafiro.ProgressReporting;
+
+namespace Zafiro.FileSystem;
+
+public class RightOnlyAction : ISyncAction
+{
+    private readonly IZafiroFile file;
+
+    public RightOnlyAction(IZafiroFile file)
+    {
+        this.file = file;
+    }
+
+    public IZafiroFile Source => file;
+    public IObservable<RelativeProgress<long>> Progress => Observable.Return(new RelativeProgress<long>(1, 1));
+    public IObservable<Result> Sync()
+    {
+        return Observable.FromAsync(() => file.Delete());
+    }
+}
