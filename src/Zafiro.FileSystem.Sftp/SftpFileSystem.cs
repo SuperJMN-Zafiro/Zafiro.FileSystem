c5e546d45fb2e26a01c599d5b0a1fac1ca04ff15
José Manuel Nieto <superjmn@outlook.com>
2023-08-03 16:32:01 +0200

    SFTP Filesystem (#4)
    Add the Secure File Transfor Protocol implementation.

---
+++ src/Zafiro.FileSystem.Sftp/SftpFileSystem.cs
@@ -0,0 +1,39 @@
+﻿using CSharpFunctionalExtensions;
+using Renci.SshNet;
+
+namespace Zafiro.FileSystem.Sftp;
+
+public class SftpFileSystem : IFileSystem, IDisposable
+{
+    private readonly SftpClient scpClient;
+
+    private SftpFileSystem(SftpClient scpClient)
+    {
+        this.scpClient = scpClient;
+    }
+
+    public void Dispose()
+    {
+        scpClient.Dispose();
+    }
+
+    public Task<Result<IZafiroDirectory>> GetDirectory(ZafiroPath path)
+    {
+        return Task.FromResult(Result.Try<IZafiroDirectory>(() => new SftpDirectory(path, scpClient)));
+    }
+
+    public Task<Result<IZafiroFile>> GetFile(ZafiroPath path)
+    {
+        return Task.FromResult(Result.Try<IZafiroFile>(() => new SftpFile(path, scpClient)));
+    }
+
+    public static Task<Result<SftpFileSystem>> Create(string host, int port, string username, string password)
+    {
+        return Task.FromResult(Result.Try(() =>
+        {
+            var client = new SftpClient(host, port, username, password);
+            client.Connect();
+            return new SftpFileSystem(client);
+        }));
+    }
+}
---
+++ test/Zafiro.FileSystem.Tests/IntegrationTest.cs
@@ -0,0 +1,19 @@
+﻿using CSharpFunctionalExtensions;
+using Zafiro.FileSystem.Sftp;
+using Zafiro.IO;
+
+namespace Zafiro.FileSystem.Tests;
+
+public class IntegrationTest
+{
+    [Fact]
+    public async Task Test()
+    {
+        var sftp = await SftpFileSystem.Create("192.168.1.29", 22, "jmn", "fillme");
+        var result = await sftp.Map(system => system.GetFile("/home/jmn/dotnet-install.sh"));
+        var file = await result.Map(fs => fs.GetContents()).Map(stream => stream.ReadToEnd());
+
+        var dir = await sftp.Map(system => system.GetDirectory("/home/jmn/seaweedfs-csi-driver"))
+            .Map(directory => directory.GetFilesInTree());
+    }
+}
