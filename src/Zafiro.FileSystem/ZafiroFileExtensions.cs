f6b968d396ccc8b6bbb06297bc1788c2ba7681db
José Manuel Nieto <superjmn@outlook.com>
2023-08-10 13:08:36 +0200

    Improve readability (#9)

--- src/Zafiro.FileSystem/ZafiroFileExtensions.cs
+++ src/Zafiro.FileSystem/ZafiroFileExtensions.cs
@@ -10,33 +10,44 @@ public static class ZafiroFileExtensions
 {
     public static Task<Result> Copy(this IZafiroFile source, IZafiroFile destination, Maybe<IObserver<RelativeProgress<long>>> progress, TimeSpan? readTimeout = default)
     {
-        return GetStream(source)
-            .Bind(async stream => await Observable.FromAsync(() => CopyToStream(destination, stream, progress)).RetryWithBackoffStrategy());
+        return GetStream(source, readTimeout).Bind(sourceStream => CopyWithRetries(sourceStream, destination, progress));
     }
 
-    private static async Task<Result> CopyToStream(IZafiroFile destination, ObservableStream stream, Maybe<IObserver<RelativeProgress<long>>> progress)
+    private static async Task<Result> CopyWithRetries(ObservableStream sourceStream, IZafiroFile destination, Maybe<IObserver<RelativeProgress<long>>> progress)
     {
-        var maybeSubscription = progress.Map(observer => stream.Positions.Select(l => new RelativeProgress<long>(stream.Length, l)).Subscribe(observer));
-        var result = await destination.SetContents(stream);
+        return await Observable
+            .FromAsync(() => CopyStreamToFile(sourceStream, destination, progress))
+            .RetryWithBackoffStrategy();
+    }
+
+    private static async Task<Result> CopyStreamToFile(ObservableStream sourceStream, IZafiroFile destinationFile, Maybe<IObserver<RelativeProgress<long>>> progress)
+    {
+        var maybeSubscription = progress.Map(observer => sourceStream.Positions.Select(l => new RelativeProgress<long>(sourceStream.Length, l)).Subscribe(observer));
+        var result = await destinationFile.SetContents(sourceStream);
         maybeSubscription.Execute(x => x.Dispose());
         return result;
     }
 
-    private static Task<Result<ObservableStream>> GetStream(IZafiroFile zafiroFile)
+    private static Task<Result<ObservableStream>> GetStream(IZafiroFile zafiroFile, TimeSpan? readTimeout = default)
     {
         var streamResult = zafiroFile.GetContents();
+        var timeoutAfter = readTimeout ?? TimeSpan.FromDays(1);
 
         return streamResult
-            .Map(stream => new ReadTimeOutStream(stream) { ReadTimeout = (int)TimeSpan.FromSeconds(15).TotalMilliseconds })
-            .Bind(async stream =>
-            {
-                if (stream.CanSeek)
-                {
-                    return new ObservableStream(stream);
-                }
+            .Map(stream => new ReadTimeOutStream(stream) { ReadTimeout = (int)timeoutAfter.TotalMilliseconds })
+            .Bind(stream => GetCompatibleStream(zafiroFile, stream));
+    }
 
-                var size = await zafiroFile.Size();
-                return size.Map(l => new ObservableStream(new AlwaysForwardStream(stream, l)));
-            });
+    private static async Task<Result<ObservableStream>> GetCompatibleStream(IZafiroFile zafiroFile, Stream stream)
+    {
+        var timingOutStream = new ReadTimeOutStream(stream);
+
+        if (timingOutStream.CanSeek)
+        {
+            return new ObservableStream(timingOutStream);
+        }
+
+        var size = await zafiroFile.Size();
+        return size.Map(l => new ObservableStream(new AlwaysForwardStream(timingOutStream, l)));
     }
 }
