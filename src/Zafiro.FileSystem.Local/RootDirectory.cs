4919523abb2e90d4eed68e7af1847b5f35ce3c1c
José Manuel Nieto <superjmn@outlook.com>
2023-10-23 13:20:55 +0200

    Add root directory in Windows

---
+++ src/Zafiro.FileSystem.Local/RootDirectory.cs
@@ -0,0 +1,32 @@
+﻿using Serilog;
+
+namespace Zafiro.FileSystem.Local;
+
+public class RootDirectory : IZafiroDirectory
+{
+    private readonly System.IO.Abstractions.IFileSystem fileSystem;
+    private readonly LocalFileSystem localFileSystem;
+    private readonly Maybe<ILogger> logger;
+
+    public RootDirectory(System.IO.Abstractions.IFileSystem fileSystem, LocalFileSystem localFileSystem, Maybe<ILogger> logger)
+    {
+        this.fileSystem = fileSystem;
+        this.localFileSystem = localFileSystem;
+        this.logger = logger;
+    }
+
+    public ZafiroPath Path => ZafiroPath.Empty;
+    public IFileSystem FileSystem => localFileSystem;
+    public Task<Result<IEnumerable<IZafiroDirectory>>> GetDirectories()
+    {
+        var result = Task.FromResult(Result
+            .Try(() => fileSystem.DriveInfo.GetDrives())
+            .Map(drives => drives.Select(info => (IZafiroDirectory)new LocalDirectory(info.RootDirectory, logger, localFileSystem))));
+
+        return result;
+    }
+
+    public Task<Result<IEnumerable<IZafiroFile>>> GetFiles() => Task.FromResult(Result.Success(Enumerable.Empty<IZafiroFile>()));
+
+    public Task<Result> Delete() => Task.FromResult(Result.Failure("Cannot delete root"));
+}
