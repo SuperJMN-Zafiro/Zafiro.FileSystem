e2f447298d9990e8430fb81481d485102e9622d9
José Manuel Nieto <superjmn@outlook.com>
2023-12-04 23:43:16 +0100

    Vamos con todo

---
+++ src/Zafiro.FileSystem.Local/LocalFileSystem2.cs
@@ -0,0 +1,39 @@
+﻿using System.Reactive.Linq;
+using Zafiro.IO;
+using Zafiro.Mixins;
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+namespace Zafiro.FileSystem.Local;
+
+public class LocalFileSystem2
+{
+    private readonly System.IO.Abstractions.IFileSystem fileSystem;
+
+    public LocalFileSystem2(System.IO.Abstractions.IFileSystem fileSystem)
+    {
+        this.fileSystem = fileSystem;
+    }
+
+    public async Task<Result> CreateFile(ZafiroPath path)
+    {
+        return Result.Try(() => fileSystem.File.Create(path));
+    }
+
+    public async Task<Result<IObservable<byte>>> GetFileContents(ZafiroPath path)
+    {
+        return Result.Try(() => Observable.Using(() => fileSystem.File.OpenRead(path), f => f.ToObservable()));
+    }
+
+    public Task<Result> SetFileContents(ZafiroPath path, IObservable<byte> bytes)
+    {
+        return Result
+            .Try(() => fileSystem.File.Open(path, FileMode.Create))
+            .Bind(stream => Result.Try(async () =>
+            {
+                using (stream)
+                {
+                    await bytes.ToStream().CopyToAsync(stream);
+                }
+            }));
+    }
+}
---
+++ test/Zafiro.FileSystem.Tests/Local/FileSystemTests.cs
@@ -0,0 +1,52 @@
+﻿using System.IO.Abstractions.TestingHelpers;
+using System.Reactive.Linq;
+using System.Text;
+using Zafiro.FileSystem.Local;
+
+namespace Zafiro.FileSystem.Tests.Local;
+
+public class FileSystemTests
+{
+    [Fact]
+    public async Task Create_file()
+    {
+        var mockFileSystem = new MockFileSystem();
+        var sut = new LocalFileSystem2(mockFileSystem);
+        var result = await sut.CreateFile("Pepito.txt");
+        result.Should().Succeed();
+        mockFileSystem.GetFile("Pepito.txt").Contents.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task Get_file()
+    {
+        var mockFileSystem = new MockFileSystem(new Dictionary<string, MockFileData>()
+        {
+            ["Pepito.txt"] = new("Salute")
+        });
+        var sut = new LocalFileSystem2(mockFileSystem);
+        var result = await sut.GetFileContents("Pepito.txt");
+
+        var bytes = result.Should().Succeed().And.Subject.Value.ToList().GetAwaiter().GetResult().ToArray();
+        Encoding.UTF8.GetString(bytes).Should().Be("Salute");
+    }
+
+    [Fact]
+    public async Task Set_file_contents()
+    {
+        var fs = new MockFileSystem(new Dictionary<string, MockFileData>()
+        {
+            ["Pepito.txt"] = new("Old content")
+        });
+
+        //var fs = new System.IO.Abstractions.FileSystem();
+        //using (fs.File.Create("Pepito.txt")){ }
+
+        var sut = new LocalFileSystem2(fs);
+        IObservable<byte> toWrite = "Salute"u8.ToArray().ToObservable();
+        var result = await sut.SetFileContents("Pepito.txt", toWrite);
+
+        result.Should().Succeed();
+        fs.GetFile("Pepito.txt").TextContents.Should().Be("Salute");
+    }
+}
