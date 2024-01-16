47f4437f4293d09ec6759e9fe2b685b15b03f9b2
José Manuel Nieto <superjmn@outlook.com>
2024-01-16 13:52:17 +0100

    Readability

--- test/Zafiro.FileSystem.Tests/ZafiroFileMixinTests.cs
+++ test/Zafiro.FileSystem.Tests/ZafiroFileMixinTests.cs
@@ -12,28 +12,20 @@ namespace Zafiro.FileSystem.Tests;
 public class ZafiroFileMixinTests
 {
     [Fact]
-    public async Task CopyTest()
+    public async Task Timeout_should_be_mapped_as_result()
     {
-        try
-        {
-            var source = new TestZafiroFile();
-            var dest = new FileSystemRoot(new ObservableFileSystem(new WindowsZafiroFileSystem(new MockFileSystem()))).GetFile("Test.txt");
-            var timeoutScheduler = new TestScheduler();
-            timeoutScheduler.AdvanceBy(4);
-            var result = await source.Copy(dest, Maybe<IObserver<LongProgress>>.None, readTimeout: TimeSpan.FromTicks(2));
-            result.Should().Fail();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
+        var source = new TestZafiroFile();
+        var dest = new FileSystemRoot(new ObservableFileSystem(new WindowsZafiroFileSystem(new MockFileSystem()))).GetFile("Test.txt");
+        var timeoutScheduler = new TestScheduler();
+        timeoutScheduler.AdvanceBy(4);
+        var result = await source.Copy(dest, Maybe<IObserver<LongProgress>>.None, readTimeout: TimeSpan.FromTicks(2));
+        result.Should().Fail();
     }
 }
 
 public class TestZafiroFile : IZafiroFile
 {
-    public IObservable<byte> Contents => Observable.Never((byte) 0);
+    public IObservable<byte> Contents => Observable.Never((byte)0);
     public Task<Result<bool>> Exists { get; }
     public ZafiroPath Path { get; }
     public Task<Result<FileProperties>> Properties => Task.FromResult(Result.Success(new FileProperties(false, DateTimeOffset.MinValue, 1000)));
