94fa12792d765fed3d4030a4afad43d5e936a064
José Manuel Nieto <superjmn@outlook.com>
2024-04-05 12:30:45 +0200

    Add rooted directory and files (#33)

---
+++ src/Zafiro.FileSystem/RootedMixin.cs
@@ -0,0 +1,9 @@
+﻿namespace Zafiro.FileSystem;
+
+public static class RootedMixin
+{
+    public static IZafiroDirectory AsRooted(this IZafiroDirectory directory)
+    {
+        return new RootedDirectory(directory, directory);
+    }
+}
