a5cc924359b2537b5b1c9b354e76ef7040bccf7a
José Manuel Nieto <superjmn@outlook.com>
2024-04-11 15:36:43 +0200

    Fix build

---
+++ test/Zafiro.FileSystem.Tests/LightweighTests.cs
@@ -0,0 +1,30 @@
+﻿using Zafiro.FileSystem.Lightweight;
+using Zafiro.Mixins;
+using Directory = Zafiro.FileSystem.Lightweight.Directory;
+using File = Zafiro.FileSystem.Lightweight.File;
+
+namespace Zafiro.FileSystem.Tests;
+
+public class LightweighTests
+{
+    [Fact]
+    public async Task Refactor_this()
+    {
+        var subdirs = new List<IDirectory>
+        {
+            new Directory("Subdir",
+            [
+                new File("File3.txt", () => Task.FromResult(Result.Success("".ToStream()))),
+                new File("File4.txt", () => Task.FromResult(Result.Success("".ToStream())))
+            ], new List<IDirectory>())
+        };
+        var sut =
+            new Directory("root", [
+                    new File("File1.txt", () => Task.FromResult(Result.Success("".ToStream()))),
+                    new File("File2.txt", () => Task.FromResult(Result.Success("".ToStream())))
+                ], 
+                subdirs);
+
+        var allDirs = await sut.GetFilesInTree("root");
+    }
+}
