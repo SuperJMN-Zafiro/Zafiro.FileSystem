71ee1c4f4b67572175bcb2e85d266e97374758aa
José Manuel Nieto <superjmn@outlook.com>
2024-04-13 00:59:45 +0200

    Add RootedFile

---
+++ src/Zafiro.FileSystem.Lightweight/RootedFile.cs
@@ -0,0 +1,13 @@
+﻿namespace Zafiro.FileSystem.Lightweight;
+
+public class RootedFile
+{
+    public RootedFile(ZafiroPath path, IFile file)
+    {
+        Path = path;
+        File = file;
+    }
+
+    public ZafiroPath Path { get; }
+    public IFile File { get; }
+}
