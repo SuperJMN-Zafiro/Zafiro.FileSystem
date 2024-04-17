046395b78d702dda2b60f07be605d2a5b8c3d3ea
José Manuel Nieto <superjmn@outlook.com>
2024-04-17 13:16:01 +0200

    Properly add vnext

---
+++ src/Zafiro.FileSystem.VNext/FileSystemRepository.cs
@@ -0,0 +1,90 @@
+﻿using System.IO.Abstractions;
+using CSharpFunctionalExtensions;
+using Zafiro.FileSystem.Lightweight;
+using Zafiro.Mixins;
+using IDirectory = Zafiro.FileSystem.Lightweight.IDirectory;
+using IFile = Zafiro.FileSystem.Lightweight.IFile;
+
+namespace Zafiro.FileSystem.VNext;
+
+public class FileSystemRepository : IFileRepository
+{
+    private readonly IFileSystem fileSystem;
+
+    public FileSystemRepository(IFileSystem fileSystem)
+    {
+        this.fileSystem = fileSystem;
+    }
+
+    public Task<Result<Maybe<IFile>>> GetFile(ZafiroPath path)
+    {
+        return GetFiles(path.Parent()).Map(files => files.TryFirst(file => file.Name == path.Name()));
+    }
+
+    public Task<Result<Maybe<IDirectory>>> GetDirectory(ZafiroPath path)
+    {
+        return GetDirectories(path.Parent()).Map(files => files.TryFirst(file => file.Name == path.Name()));
+    }
+
+    public Task<Result<IEnumerable<IFile>>> GetFiles(ZafiroPath path)
+    {
+        return Task.FromResult(Result.Try(() =>
+        {
+            var dir = fileSystem.DirectoryInfo.New(path);
+            return dir.GetFiles()
+                .Select(fileInfo => (IFile) new RootedFile(dir.FullName.Replace("\\", "/"), new SystemIOFile(fileInfo)));
+        }));
+    }
+
+    public Task<Result<IEnumerable<IDirectory>>> GetDirectories(ZafiroPath path)
+    {
+        return Task.FromResult(Result.Try(() =>
+        {
+            var rootDir = path == ZafiroPath.Empty ? fileSystem.DriveInfo.GetDrives().Select(r => r.RootDirectory) : fileSystem.DirectoryInfo.New(path).GetDirectories();
+        
+            return rootDir
+                .Select(s => (IDirectory) new SystemIODirectory(s)); 
+        }));
+    }
+
+    public async Task<Result<IFile>> AddOrUpdate(ZafiroPath path, IFile data)
+    {
+        var result = path.Combine(data.Name);
+        return await ResultMixin.Using(data.Open(), stream => WriteStream(result.ToString().Replace("/", "\\"), stream))
+            .Bind(() => GetFile(result)
+                .Bind(r => r.ToResult("Could not retrieve the file")));
+    }
+    
+    public async Task WriteStream(string ruta, Stream stream)
+    {
+        // Extraer el directorio padre de la ruta proporcionada
+        var directorio = Path.GetDirectoryName(ruta);
+
+        // Verificar si el directorio existe y crearlo si no existe
+        if (!fileSystem.Directory.Exists(directorio))
+        {
+            fileSystem.Directory.CreateDirectory(directorio);
+        }
+
+        // Crear el archivo en la ruta especificada, FileMode.Create crea un nuevo archivo
+        // o sobrescribe uno existente
+        using (var fs = fileSystem.File.Create(ruta))
+        {
+            await stream.CopyToAsync(fs);
+        }
+    }
+}
+
+public static class ResultMixin
+{
+    public static async Task<Result> Using(this Task<Result<Stream>> streamResult, Func<Stream, Task> useStream)
+    {
+        return await streamResult.Tap(async stream =>
+        {
+            await using (stream)
+            {
+                await useStream(stream);
+            }
+        });
+    }
+}
---
+++ src/Zafiro.FileSystem.VNext/IFileRepository.cs
@@ -0,0 +1,12 @@
+﻿using CSharpFunctionalExtensions;
+using Zafiro.FileSystem.Lightweight;
+
+namespace Zafiro.FileSystem.VNext
+{
+    public interface IFileRepository
+    {
+        Task<Result<IEnumerable<IFile>>> GetFiles(ZafiroPath path);
+        Task<Result<IEnumerable<IDirectory>>> GetDirectories(ZafiroPath path);
+        Task<Result<IFile>> AddOrUpdate(ZafiroPath path, IFile data);
+    }
+}
