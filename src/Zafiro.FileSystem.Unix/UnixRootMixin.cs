1b4f9c35b96f9b40553ceba18e8f19321467c967
José Manuel Nieto <superjmn@outlook.com>
2024-05-14 00:36:56 +0200

    Parece que la cosa chusca

--- src/Zafiro.FileSystem.Unix/UnixRootMixin.cs
+++ src/Zafiro.FileSystem.Unix/UnixRootMixin.cs
@@ -5,7 +5,7 @@ public static class UnixRootMixin
     public static UnixNode FromRootedFiles(this ICollection<RootedUnixFile> allFiles, ZafiroPath parent)
     {
         var files = allFiles.Where(x => x.Parent == parent).Select(x => (UnixNode)x.UnixFile);
-        var nextLevelPaths = allFiles.Where(x => x.Parent.Parent() == parent).Select(x => x.Parent).ToList();
+        var nextLevelPaths = allFiles.Where(x => x.Parent.Parent() == parent).Select(x => x.Parent).ToList().Distinct();
         var dirs = nextLevelPaths.Select(x => FromRootedFiles(allFiles, x)).ToList();
         var unixNodes = files.Concat(dirs);
         return new UnixDir(parent.Name(), unixNodes);
