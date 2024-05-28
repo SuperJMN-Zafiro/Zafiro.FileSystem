c82ef671dbd0b0271a79539ca57a4ffd59a43a63
José Manuel Nieto <superjmn@outlook.com>
2024-05-28 16:45:10 +0200

    Vámonos al wip

---
+++ src/Zafiro.FileSystem.Mutable/Class1.cs
@@ -0,0 +1,22 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace Zafiro.FileSystem.Mutable;
+
+public class Class1
+{
+
+}
+
+public interface IMutableDirectory : IMutableNode
+{
+    Task<Result<IEnumerable<IMutableNode>>> MutableChildren();
+
+}
+
+public interface IMutableFile : IFile, IMutableNode
+{
+}
+
+public interface IMutableNode
+{
+}
