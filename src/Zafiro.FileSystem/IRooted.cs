9bc694936930331f424b227096e4d4174e11e1c1
José Manuel Nieto <superjmn@outlook.com>
2024-06-04 10:56:25 +0200

    Improvements

--- src/Zafiro.FileSystem/IRooted.cs
+++ src/Zafiro.FileSystem/IRooted.cs
@@ -1,7 +1,11 @@
 ﻿namespace Zafiro.FileSystem;
 
-public interface IRooted<out T>
+public interface IRooted
 {
     ZafiroPath Path { get; }
+}
+
+public interface IRooted<out T> : IRooted
+{
     T Value { get; }
 }
