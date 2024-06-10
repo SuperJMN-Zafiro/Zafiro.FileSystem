8d0aed3d6e9689e5d32b2adf19a3f6a6b726529a
José Manuel NIeto Sánchez (SuperJMN) <superjmn@outlook.com>
2024-06-10 23:25:54 +0200

    Remove FileSystemRepository and update tests
    Deleted the FileSystemRepository class as it's no longer needed. Corresponding unit tests in FileSystemRepositoryTests.cs have also been updated to reflect the removal. Added a new dependency on DynamicData, which is now being used in tests, in Zafiro.FileSystem.VNext.csproj.

--- test/Zafiro.FileSystem.VNext.Tests/FileSystemRepositoryTests.cs
+++ test/Zafiro.FileSystem.VNext.Tests/FileSystemRepositoryTests.cs
@@ -1,29 +1,87 @@
+using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using DynamicData;
+using Zafiro.FileSystem.Local;
+using Zafiro.Reactive;
 
 namespace Zafiro.FileSystem.VNext.Tests;
 
 public class FileSystemRepositoryTests
 {
     [Fact]
-    public async Task Create_new_file()
+    public async Task DDTest()
     {
-        var mockFileSystem = new MockFileSystem();
-        var fs = new FileSystemRepository(mockFileSystem);
-        var file = await fs.AddOrUpdate("c:/Subdir", new File("Test1.txt", "Contenido"));
-        file.Should().Succeed();
-        mockFileSystem.GetFile("C:\\Subdir\\Test1.txt").TextContents.Should().Be("Contenido");
-    }
+        var fs = new LocalFileSystem(new System.IO.Abstractions.FileSystem());
+        var folder = fs.GetFolder("home/jmn/Escritorio");
+
+
+        folder.Files.OnItemAdded(file => { }).Subscribe();
         
-    [Fact]
-    public async Task Update_file()
+        var result = await folder.AddOrUpdate(new[] { new File("Pepito.txt", "Hehehe") });
+        await Task.Delay(10000);
+        result.Should().Succeed();
+    }
+}
+
+public class LocalFileSystem(IFileSystem fileSystem)
+{
+    public IFileSystem FileSystem { get; } = fileSystem;
+
+    public ObservableFolder GetFolder(ZafiroPath path)
     {
-        var mockFileSystem = new MockFileSystem(new Dictionary<string, MockFileData>()
-        {
-            ["C:\\Subdir\\Test1.txt"] = new MockFileData("Contenido"),
-        });
-        var fs = new FileSystemRepository(mockFileSystem);
-        var file = await fs.AddOrUpdate("c:/Subdir", new File("Test1.txt", "Nuevo contenido"));
-        file.Should().Succeed();
-        mockFileSystem.GetFile("C:\\Subdir\\Test1.txt").TextContents.Should().Be("Nuevo contenido");
+        return new ObservableFolder(FileSystem.DirectoryInfo.New("/" + path));
+    }
+}
+
+public class ObservableFolder
+{
+    private readonly SourceCache<IFile,string> filesCache;
+    private readonly SourceCache<ObservableFolder,string> dirsCache;
+    public IDirectoryInfo DirectoryInfo { get; }
+
+    public ObservableFolder(IDirectoryInfo directoryInfo)
+    {
+        DirectoryInfo = directoryInfo;
+        filesCache =  new SourceCache<IFile, string>(x => x.Name);
+        //dirsCache =  new SourceCache<ObservableFolder, string>(x => x.Name);
+        
+        filesCache.AddOrUpdate(directoryInfo.GetFiles().Select(info => new DotNetFile(info)));
+        //dirsCache.AddOrUpdate(directoryInfo.GetDirectories().Select(info => new ObservableFolder(info)));
+
+        Files = Observable
+            .Timer(TimeSpan.FromSeconds(2))
+            .Select(_ => Observable.Start(() => DirectoryInfo.GetFiles().Select(d => (IFile)new DotNetFile(d)), TaskPoolScheduler.Default))
+            .Repeat()
+            .Concat().EditDiff(x => x.Name);
+        
+        //Directories = dirsCache.Connect();
+        //AllDirs = Directories.MergeManyChangeSets(folder => folder.Directories);
+        //AllFiles = AllDirs.MergeManyChangeSets(x => x.Files);
+    }
+    
+    public IObservable<IChangeSet<IFile,string>> AllFiles { get; }
+
+    public IObservable<IChangeSet<ObservableFolder,string>> AllDirs { get; }
+
+    public IObservable<IChangeSet<ObservableFolder,string>> Directories { get; }
+
+    public IObservable<IChangeSet<IFile, string>> Files { get; }
+
+    public string Name => DirectoryInfo.Name;
+
+    public Task<Result> AddOrUpdate(File[] files)
+    {
+        return files.Select(AddOrUpdate).Combine();
+    }
+
+    private async Task<Result> AddOrUpdate(File file)
+    {
+        var path = DirectoryInfo.FileSystem.Path.Combine(DirectoryInfo.FullName, file.Name);
+        await using var fileSystemStream = DirectoryInfo.FileSystem.File.Create(path);
+        var result = await file.DumpTo(fileSystemStream);
+        result.Tap(() => filesCache.AddOrUpdate(file));
+        return result;
     }
 }
