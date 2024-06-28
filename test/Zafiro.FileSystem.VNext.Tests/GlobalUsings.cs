5361e2587a7d1817edd48994968cd22ac2f3d5bf
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2024-06-28 19:41:33 +0200

    +semver: major

--- test/Zafiro.FileSystem.VNext.Tests/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-// Global using directives
-
-global using CSharpFunctionalExtensions;
-global using FluentAssertions;
-global using Xunit;
-global using Directory = Zafiro.FileSystem.Readonly.Directory;
-global using File = Zafiro.FileSystem.Readonly.File;
-global using IFile = Zafiro.FileSystem.Readonly.IFile;
